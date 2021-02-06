38b4107d15ba5b9f7d563a6fa488d592e5ffdf83
romulo.palaoro <romulopalaoro@gmail.com>
2021-02-06 16:51:04 -0300

    css error

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/Header'
 import Main from './components/Main'
+// eslint-disable-next-line
 import css from '../src/App.css'
 
 function App() {
