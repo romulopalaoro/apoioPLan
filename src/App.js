d814d2c7666d419002b46ab04677628b422d15fe
romulo.palaoro <romulopalaoro@gmail.com>
2021-02-11 19:20:06 -0300

    inclusão equipamentos

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React from 'react';
-import Header from './components/Header'
+
 import Main from './components/Main'
 // eslint-disable-next-line
-import css from '../src/App.css'
+import css from './App.css'
 
 function App() {
   return (
-    <div className="container">
-      <Header >
-      </Header>
-      <Main>
-      </Main>
-    </div>
+      <Main/>
   );
 }
 export default App;
--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
+
 import db from '../../dbPlan.json'
+import dbEq from '../../dbPlanEquip.json'
 
 const operations = db.operations.map(op=>{
   return (
@@ -9,9 +11,17 @@ const operations = db.operations.map(op=>{
     }
   )})
 
+  const equipamentos = dbEq.operations.map(op=>{
+    return (
+      {
+        "operation": op.textobutton,
+        "description": op.textoOperation
+      }
+    )})
+
 export default function Main() {
   
-  function handleCopyOperation(event){
+  function handleCopyOperation(){
     let op = document.querySelector("textarea")
     console.log(op)
     // let op1 = toString(event.target.nextSibling.textContent)
@@ -19,30 +29,61 @@ export default function Main() {
     op.select()
     document.execCommand('copy')
   }
+
   function handleOperations(event){
-    setInformations(event.target.value)
+      setInformations(event.target.value)
   }
 
+  function handleEscolha(event){
+   const esc = event.target.value;
+   setEscolha(esc)
+   console.log(esc)
+  }
+  
+
   const [informations, setInformations ] = React.useState();
+  const [escolha, setEscolha] = React.useState("OM Vermelha");
+
+  React.useEffect(() => {
+   
+  }, [informations, escolha])
 
   return (
+    <div className="container">
+    <div className="header">
+      <h1>Apoio ao planejamento</h1>
+      <button onClick={handleEscolha} value="OM Vermelha">OM Vermelha</button>
+      <button onClick={handleEscolha} value="Equipamentos">Equipamentos</button>
+      
+    </div>
     <div className="main">
       <div className="sidebar">
-          {operations.map(op=>{
+        {escolha === "OM Vermelha" ?
+          operations.map(op=>{
             return (
-              <div >
+              <div key={op.description}>
                 <button className='buttons' onClick={handleOperations} value={op.description}>
                   {op.operation}
                 </button>
               </div>
               )
               })
+          : (equipamentos.map(eq=>{
+            return (
+              
+                <button key={eq.operation} className='buttons' onClick={handleOperations} value={eq.description}>
+                  {eq.operation}
+                </button>
+              
+              )
+          }))
           }
       </div>
       <div className="buttonAndTextarea">
         <button onClick={handleCopyOperation} className="buttonCopy" id="input">COPIAR TEXTO</button>
-        <textarea id="input" className='descriptions' value={informations}/>
+        <textarea id="input" className='descriptions' value={informations} />
       </div>
+    </div>
     </div>
   )
 }
